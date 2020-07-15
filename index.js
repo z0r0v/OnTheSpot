28e00df667b24949fe799021285e09fa52444280
Your Name <3767082@mail.ru>
2020-07-15 18:03:34 +0300

    added help function

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-
 // get name;
 // get coast;
 // get original coast;
 // ling mane img;
 // Array all img;
 
-const getInnerText = (element)=> {
+const getInnerText = (element) => {
     return document.querySelector(element).innerText;
 };
 
+const dqs = (element, all) => {
+    if (all === false) {
+        return document.querySelector(element);
+    } else {
+        return document.querySelectorAll(element);
+    }
+};
+
+const getArray = (element) => {
+    return Array.prototype.slice.call(element);
+};
+
 const getUrlImg = (element) => {
-const lists = document.querySelectorAll(element);
-const arrayList =  Array.prototype.slice.call(lists);
-const newList = arrayList.filter(list => !list.ariaHidden);
-const arrayChild =  Array.prototype.slice.call(newList[0].children);
-return arrayChild[1].src
+    const lists = dqs(element, 0);
+    const arrayList = getArray(lists);
+    const newList = arrayList.filter(list => !list.ariaHidden);
+    const arrayChild = getArray(newList[0].children);
+    return arrayChild[1].src
 };
+
 const getArrayImg = (element) => {
-    const imgList = document.querySelectorAll(element);
-    return Array.prototype.slice.call(imgList)
+    const imgList = dqs(element, 1);
+    return getArray(imgList)
 };
-const origCoast = getInnerText(".product-price").split('$')[0] + '$';
+
+
 console.log(
-    "Name:", getInnerText(".product-hero h1"),'\n',
-    "Coast:", getInnerText(".product-price-discounted"),'\n',
-    "Original Coast:", origCoast, '\n',
-    "LinkImage:", getUrlImg('.image-container.zoomable'),'\n',
+    "Name:", getInnerText(".product-hero h1"), '\n',
+    "Coast:", getInnerText(".product-price-discounted"), '\n',
+    "Original Coast:", getInnerText(".product-price").split('$')[0] + '$', '\n',
+    "LinkImage:", getUrlImg('.image-container.zoomable'), '\n',
     "Array Img:", getArrayImg('.image-container .gallery-image'),
 );
 

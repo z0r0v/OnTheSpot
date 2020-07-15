43687136209cbaf4c2f2abb375aa6b077222f320
Your Name <3767082@mail.ru>
2020-07-15 17:33:37 +0300

    uploaded test logik

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ const getInnerText = (element)=> {
     return document.querySelector(element).innerText;
 };
 const getUrlImg = (element) => {
-    return document.querySelector(element).src
+const lists = document.querySelectorAll(element);
+const arrayList =  Array.prototype.slice.call(lists);
+const newList = arrayList.filter(list => !list.ariaHidden);
+const arrayChild =  Array.prototype.slice.call(newList[0].children);
+console.log(arrayChild[1].src);
 };
 const getArrayImg = (element) => {
     const imgList = document.querySelectorAll(element);
     return Array.prototype.slice.call(imgList)
 };
-
 const origCoast = getInnerText(".product-price").split('$')[0] + '$';
-
 console.log(
-
     "Name:", getInnerText(".product-hero h1"),'\n',
     "Coast:", getInnerText(".product-price-discounted"),'\n',
     "Original Coast:", origCoast, '\n',
-    "LinkImage:", getUrlImg('.fullImageContainer .img'),'\n',
+    "LinkImage:", getUrlImg('.image-container.zoomable'),'\n',
     "Array Img:", getArrayImg('.image-container .gallery-image'),
-
 );
 
 
